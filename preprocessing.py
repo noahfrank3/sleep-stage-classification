aa34fd8cdc1018850b23622ab8cc484baff3d89e
noahfrank3 <noahfrank3@outlookc.om>
2025-03-18 19:42:20 +0000

    worked on extracting edf files

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,10 @@ import numpy as np
 from scipy.fft import rfft, rfftfreq
 import matplotlib.pyplot as plt
 
+# edf extraction example
+# file = pyedflib.EdfReader('test.edf')
+# sig = file.readSignal(0)
+
 # Compute real fourier transform
 def ft(x, f_s):
     f = rfftfreq(len(x), 1/f_s)
@@ -12,16 +16,12 @@ def ft(x, f_s):
 
     return f, A
 
-sort_edfs = lambda filenames: sorted(filenames, key=lambda s: int(s[3:5]))
-
-# edf extraction example
-# file = pyedflib.EdfReader('test.edf')
-# sig = file.readSignal(0)
-
 # Sample plot of a signal
 # f, A = ft(sig, 100)
 # plt.plot(f, A)
 # plt.show()
+
+sort_edfs = lambda filenames: sorted(filenames, key=lambda s: int(s[3:5]))
 
 ## Cassette files
 os.chdir(os.path.join('sleep-edf-database-expanded-1.0.0', 'sleep-cassette'))
@@ -32,24 +32,3 @@ cassette_psgs_1 = sort_edfs([s for s in cassette_files if 'PSG' in s and s[5] ==
 cassette_psgs_2 = sort_edfs([s for s in cassette_files if 'PSG' in s and s[5] == '2'])
 cassette_hypnograms_1 = sort_edfs([s for s in cassette_files if 'Hypnogram' in s and s[5] == '1'])
 cassette_hypnograms_2 = sort_edfs([s for s in cassette_files if 'Hypnogram' in s and s[5] == '2'])
-
-n_birthdate = 0
-n_equipment = 0
-n_sex = 0
-n_technician = 0
-for filename in cassette_hypnograms_1 + cassette_hypnograms_2:
-    file = pyedflib.EdfReader(filename)
-
-    if file.getBirthdate():
-        n_birthdate += 1
-    if file.getEquipment():
-        n_equipment += 1
-    if file.getSex():
-        n_sex += 1
-    if file.getTechnician():
-        n_technician += 1
-n = len(cassette_psgs_1 + cassette_psgs_2)
-print(f'Birthdate = {100*n_birthdate/n:.3g}%')
-print(f'Equipment = {100*n_equipment/n:.3g}%')
-print(f'Sex = {100*n_sex/n:.3g}%')
-print(f'Technician = {100*n_technician/n:.3g}%')
