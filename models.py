6e2ae750507e9b31d96b850e24e9f7ecc59c3919
noahfrank3 <noahfrank3@outlookc.om>
2025-03-22 12:36:58 -0400

    Create file for storing ML models

---
+++ models.py
@@ -0,0 +1,9 @@
+# Get EEG signal
+# signal = pyedflib.highlevel.read_edf(str(cassette_path / psg_filename))[0][0]
+#
+# Sleep waves
+# Delta: 0-3.5 Hz
+# Theta: 4-7.5 Hz
+# Alpha: 8-13 Hz
+# Beta: 14-30 Hz
+# Gamma 30-100 Hz
