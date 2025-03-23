c816393f5ab8678dc76f1bb49e972c7f99295b0e
Maurer, Ian Kersting <imaurer3@gatech.edu>
2025-03-23 16:04:40 -0400

    Added SVD

--- models.py
+++ models.py
@@ -14,9 +14,10 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import TruncatedSVD
 
 
-#PCA Model selection scoring method, cite https://stackoverflow.com/questions/53556359/selecting-kernel-and-hyperparameters-for-kernel-pca-reduction
+#PCA/SVD Model selection scoring method, cite https://stackoverflow.com/questions/53556359/selecting-kernel-and-hyperparameters-for-kernel-pca-reduction
 def my_scorer(estimator, X, y=None):
     X_reduced = estimator.transform(X)
     X_preimage = estimator.inverse_transform(X_reduced)
@@ -33,3 +34,17 @@ def model_select_pca(x):
     print("Best Hyperparameters:", grid_search.best_params_)
     print("Best Score:", grid_search.best_score_)
     return x
+
+#SVD
+def SVD(x):
+    param_grid = [{"n_components": np.linspace(1, 10, 10),}]
+    trun_svd =  TruncatedSVD()
+    grid_search = GridSearchCV(trun_svd, param_grid, scoring=my_scorer)
+    x_new = grid_search.fit(x)
+    return x_new
+
+#LDA
+
+#LASSO
+
+#Random Forest
