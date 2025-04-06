b2965d0c405e08cf2286372170f7b0b02f79bb36
imaurer3 <imaurer3@gatech.edu>
2025-04-06 17:30:01 -0400

    finished hyperparameters

--- optimization/models.py
+++ optimization/models.py
@@ -12,24 +12,24 @@ import xgboost as xgb
 
 
 def PCA_wrapper(trial):
-    n_componenets = trial.suggest_int('n_componenets',2,7)
-    return PCA(n_components= n_componenets, n_jobs=-1) 
+    n_components = trial.suggest_int('n_components',1,m, log = True)
+    return PCA(n_components= n_components, n_jobs=-1) 
 
 def KPCA_wrapper(trial):
-    n_componenets = trial.suggest_int('n_componenets',2,7)
+    n_components = trial.suggest_int('n_components',1,m, log = True)
     kernel = trial.suggest_categorical('kernal',['linear', 'poly', 'rbf', 'sigmoid', 'cosine'])
-    return KernelPCA(n_components= n_componenets, kernel = kernel, n_jobs=-1)
+    return KernelPCA(n_components= n_components, kernel = kernel, n_jobs=-1)
 
 def LDA_wrapper(trial):
     solver = trial.suggest_categorical('solver', ['svd', 'lsqr', 'eigen'])
     return LinearDiscriminantAnalysis(solver = solver)
 
 def SVD_wrapper(trial):
-    n_components = trial.suggest_int('n_componenets',2,7)
+    n_components = trial.suggest_int('n_components',1,m, log = True)
     return TruncatedSVD(n_components = n_components)
 
 def Lasso_wrapper(trial):
-    alpha = trial.suggest_int('alpha',.01,20)
+    alpha = trial.suggest_int('alpha',.001,1000, log = True)
     return Lasso(alpha = alpha)
 
 def DT_wrapper(trial):
@@ -37,12 +37,12 @@ def DT_wrapper(trial):
     return DecisionTreeClassifier(criterion = criterion)
 
 def random_forest_wrapper(trial):
-    n_estimators = trial.suggest_int('n_estimators',1, 50)
+    n_estimators = trial.suggest_int('n_estimators',1, 1000, log = True)
     criterion = trial.suggest_categorical('criterion', ['gini', 'entropy', 'log_loss'])
     return RandomForestClassifier(n_estimators=n_estimators,criterion=criterion)
 
 def kNN_wrapper(trial):
-    n_neighbors = trial.suggest_int('n_neighbors', 1, 100, log=True)
+    n_neighbors = trial.suggest_int('n_neighbors', 1, n_con, log = True)
     return KNeighborsClassifier(n_neighbors=n_neighbors)
 
 def kernal_SVM_wrapper(trial):
@@ -51,13 +51,13 @@ def kernal_SVM_wrapper(trial):
 
 def xg_boost_wrapper(trial):
     booster = trial.suggest_categorical('booster', ['gbtree', 'gblinear', 'dart'])
-    max_depth = trial.suggest_int('max_depth', 1, 100)
+    max_depth = trial.suggest_int('max_depth',.001,1000,log = True)
     return xgb.XGBClassifier(booster = booster, max_depth=max_depth)
 
 def NN_wrapper(trial):
     solver = trial.suggest_categorical(solver,'lbfgs', 'sgd', 'adam')
-    alpha = trial.suggest_float('alpha',.001,.1)
-    hidden_layer_sizes = trial.suggest_int('hidden_layer_sizes',10,500)
+    alpha = trial.suggest_float('alpha',.001,1000,log = True)
+    hidden_layer_sizes = trial.suggest_int('hidden_layer_sizes',.001,1000,log = True)
     return MLPClassifier(solver = solver, alpha = alpha, hidden_layer_sizes = hidden_layer_sizes)
 
 # Maps dimensionality reduction technique to its wrapper
