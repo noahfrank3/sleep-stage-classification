cd7d3f9413332d990ded44fb18e8d80dcf0304dc
noahfrank3 <noahfrank3@outlookc.om>
2025-04-07 17:55:59 -0400

    Change number of CPUs

--- optimization/optimize.sh
+++ optimization/optimize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J sleep_stage_optimization
 #SBATCH -o out.txt
-#SBATCH --cpus-per-task=100
-#SBATCH --mem=400G
+#SBATCH --cpus-per-task=80
+#SBATCH --mem=230G
 
 module load python
 source ~/my_env/bin/activate
