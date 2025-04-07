e38ee98b8bc8810dd8d9bb7f450d0afa28eda9b4
noahfrank3 <noahfrank3@outlookc.om>
2025-04-07 14:24:45 -0400

    Configure optimization for parallel computing

--- optimization/main.py
+++ optimization/main.py
@@ -1,118 +1,18 @@
 from pathlib import Path
 from multiprocessing import cpu_count
 
-import numpy as np
-import h5py
-import optuna
-from optuna.samplers import NSGAIISampler
-from pympler import asizeof
-from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit, StratifiedKFold, train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
-from sklearn.pipeline import Pipeline
-
-from transformers import FeatureExtractor, CircularEncoder
-from models import DimReductionWrapper, CLFWrapper
-
-k = 5 # number of folds for k-fold CV
-n_workers = cpu_count() # number of CPUs to use (set to cpu_count() for HPC)
-n_trials = 100 # number of trials for optimization run
-data_path = Path('..') / 'data' # path of data directory
-
-# Loads X and Y from the h5 data file
-def load_data(data_path):
-    # Loop through each signal and label, adding to X and y respectively
-    with h5py.File(data_path / 'data.h5', 'r') as hdf:
-        X = []
-        y = []
-
-        for group in hdf.keys():
-            signals = hdf[group]
-            for signal in signals.values():
-                X.append(signal.attrs['id'])
-                y.append(signal.attrs['sleep_stage'])
-
-    X = np.array(X)
-    y = np.array(y)
-
-    # Encode labels
-    label_encoder = LabelEncoder()
-    y = label_encoder.fit_transform(y)
-
-    return X, y
-
-# Wraps train_test_split to enable persistant storage for reusing train/test split
-def train_test_split_wrapper(data_path, X, y):
-    # Check if data file exists and load indices
-    try:
-        idx = np.load(data_path / 'train_test_split_idx.npz')
-        train_idx = idx['train_idx']
-        test_idx = idx['test_idx']
-    # Create new train/test split if no data file exists
-    except FileNotFoundError:
-        train_idx, test_idx = train_test_split(np.arange(len(y)), test_size=0.2, shuffle=True, stratify=y)
-        np.savez(data_path / 'train_test_split_idx.npz', train_idx=train_idx, test_idx=test_idx)
-
-    # Split data into training/testing sets
-    X_train, X_test = X[train_idx], X[test_idx]
-    y_train, y_test = y[train_idx], y[test_idx]
-
-    return X_train, X_test, y_train, y_test
-
-# Define objective and pipeline for optimization
-def objective(trial, cv, n, X_train, y_train):
-    # Feature extraction
-    feature_extractor = FeatureExtractor(trial, n_workers)
-    m = feature_extractor.get_m()
-
-    # Encode features
-    encoder = ColumnTransformer(
-        transformers=[
-            ('scale', StandardScaler(), ['age']), 
-            ('onehot', OneHotEncoder(), ['study', 'sex']),
-            ('time', CircularEncoder(), ['lights_off'])
-        ]
-    )
-
-    # Dimensionality reduction
-    dim_reduction = DimReductionWrapper(trial, n, m)
-    dim_reduction = dim_reduction.get_dim_reduction()
-
-    # Classification
-    clf = CLFWrapper(trial, n, m)
-    clf = clf.get_clf()
-
-    # Build pipeline
-    pipeline = Pipeline([
-        ('feature_extractor', feature_extractor),
-        ('encoder', encoder),
-        ('dim_reduction', dim_reduction),
-        ('clf', clf)
-    ])
-
-    # Calculate objectives
-    cv_error =  1 - cross_val_score(pipeline, X_train, y_train, cv=cv,
-                               scoring='accuracy').mean()
-    cv_space = asizeof.asizeof(pipeline)*2**(-20) # MB
-    return cv_error, cv_space
-
-# Run optimization process
-def run_optimization(data_path):
-    # Load data
-    X, y = load_data(data_path)
-
-    # Separate training and testing data
-    X_train, X_test, y_train, y_test = train_test_split_wrapper(data_path, X, y)
-    
-    # Generate folds for k-fold CV
-    cv = StratifiedKFold(n_splits=k, shuffle=True)
-
-    # Define safe lower upper bound for n of a fold training set
-    n = (k - 1)*(len(X_train) // k)
-
-    # Create optuna study and run optimization
-    study = optuna.create_study(sampler=NSGAIISampler(), directions=['minimize', 'minimize'])
-    study.optimize(lambda trial: objective(trial, cv, n, X_train, y_train), n_trials=n_trials)
+from optimization import run_optimization
 
 if __name__ == '__main__':
-    run_optimization(data_path)
+    # Parameters
+    k = 5 # number of folds for k-fold CV
+    n_trials = 100 # number of trials for optimization run
+    n_workers = cpu_count() # number of CPUs to use (set to cpu_count() for HPC)
+    f_trial_workers = 0.65 # fraction of workers dedicated to running whole trials
+    data_path = Path('..') / 'data' # path of data directory
+
+    n_trial_workers = int(f_trial_workers*n_workers)
+    n_internal_workers = n_workers - n_trial_workers
+
+    # Run optimization
+    run_optimization(data_path, k, n_trials n_trial_workers, n_internal_workers)
---
+++ optimization/optimization.py
@@ -0,0 +1,108 @@
+import numpy as np
+import h5py
+import optuna
+from optuna.samplers import NSGAIISampler
+from pympler import asizeof
+from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+
+from transformers import FeatureExtractor, CircularEncoder
+from models import DimReductionWrapper, CLFWrapper
+
+# Loads X and Y from the h5 data file
+def load_data(data_path):
+    # Loop through each signal and label, adding to X and y respectively
+    with h5py.File(data_path / 'data.h5', 'r') as hdf:
+        X = []
+        y = []
+
+        for group in hdf.keys():
+            signals = hdf[group]
+            for signal in signals.values():
+                X.append(signal.attrs['id'])
+                y.append(signal.attrs['sleep_stage'])
+
+    X = np.array(X)
+    y = np.array(y)
+
+    # Encode labels
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
+    return X, y
+
+# Wraps train_test_split to enable persistant storage for reusing train/test split
+def train_test_split_wrapper(data_path, X, y):
+    # Check if data file exists and load indices
+    try:
+        idx = np.load(data_path / 'train_test_split_idx.npz')
+        train_idx = idx['train_idx']
+        test_idx = idx['test_idx']
+    # Create new train/test split if no data file exists
+    except FileNotFoundError:
+        train_idx, test_idx = train_test_split(np.arange(len(y)), test_size=0.2, shuffle=True, stratify=y)
+        np.savez(data_path / 'train_test_split_idx.npz', train_idx=train_idx, test_idx=test_idx)
+
+    # Split data into training/testing sets
+    X_train, X_test = X[train_idx], X[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+
+    return X_train, X_test, y_train, y_test
+
+# Define objective and pipeline for optimization
+def objective(trial, cv, n, X_train, y_train, n_internal_workers):
+    # Feature extraction
+    feature_extractor = FeatureExtractor(trial, n_internal_workers)
+    m = feature_extractor.get_m()
+
+    # Encode features
+    encoder = ColumnTransformer(
+        transformers=[
+            ('scale', StandardScaler(), ['age']), 
+            ('onehot', OneHotEncoder(), ['study', 'sex']),
+            ('time', CircularEncoder(), ['lights_off'])
+        ]
+    )
+
+    # Dimensionality reduction
+    dim_reduction = DimReductionWrapper(trial, n, m)
+    dim_reduction = dim_reduction.get_dim_reduction()
+
+    # Classification
+    clf = CLFWrapper(trial, n, m)
+    clf = clf.get_clf()
+
+    # Build pipeline
+    pipeline = Pipeline([
+        ('feature_extractor', feature_extractor),
+        ('encoder', encoder),
+        ('dim_reduction', dim_reduction),
+        ('clf', clf)
+    ])
+
+    # Calculate objectives
+    cv_error =  1 - cross_val_score(pipeline, X_train, y_train, cv=cv,
+                               scoring='accuracy').mean()
+    cv_space = asizeof.asizeof(pipeline)*2**(-20) # MB
+    return cv_error, cv_space
+
+# Run optimization process
+def run_optimization(data_path, k, n_trials, n_trial_workers, n_internal_workers):
+    # Load data
+    X, y = load_data(data_path)
+
+    # Separate training and testing data
+    X_train, X_test, y_train, y_test = train_test_split_wrapper(data_path, X, y)
+    
+    # Generate folds for k-fold CV
+    cv = StratifiedKFold(n_splits=k, shuffle=True)
+
+    # Define safe lower upper bound for n of a fold training set
+    n = (k - 1)*(len(X_train) // k)
+
+    # Create optuna study and run optimization
+    optuna.logging.set_verbosity(optuna.logging.INFO)
+    study = optuna.create_study(sampler=NSGAIISampler(), directions=['minimize', 'minimize'])
+    study.optimize(lambda trial: objective(trial, cv, n, X_train, y_train, n_internal_workers), n_trials=n_trials, n_jobs=n_trial_workers)
