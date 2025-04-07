5cec584ba8bf4eb83a0f231082dbe2794df5b40f
noahfrank3 <noahfrank3@outlookc.om>
2025-04-07 12:46:18 -0400

    Change memory metric to storage space

--- optimization/main.py
+++ optimization/main.py
@@ -6,7 +6,8 @@ import h5py
 from memory_profiler import memory_usage
 import optuna
 from optuna.samplers import NSGAIISampler
-from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold
+from pympler import asizeof
+from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit, StratifiedKFold
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
@@ -78,34 +79,11 @@ def objective(trial, cv, n, X_trainval, y_trainval):
         ('clf', clf)
     ])
 
-    # K-fold cross-validation
-    return evaluate_objectives(pipeline, cv, X_trainval, y_trainval)
-
-def evaluate_objectives(pipeline, cv, X_trainval, y_trainval):
-    cv_error, cv_memory = np.empty(k), np.empty(k)
-
-    for idx, (train_idx, val_idx) in enumerate(cv.split(X_trainval, y_trainval)):
-        # Split data into training and validation sets
-        X_train, y_train = X_trainval[train_idx], y_trainval[train_idx]
-        X_val, y_val = X_trainval[val_idx], y_trainval[val_idx]
-        
-        # Calculate objectives for this fold
-        error, memory = evaluate_fold_objectives(pipeline, X_train, y_train, X_val, y_val)
-        
-        cv_error[idx] = error
-        cv_memory[idx] = memory
-
-    return np.mean(cv_error), np.mean(cv_memory)
-
-def evaluate_fold_objectives(pipeline, X_train, y_train, X_val, y_val):
-    # Train model
-    pipeline.fit(X_train, y_train)
-
     # Calculate objectives
-    error = 1 - pipeline.score(X_val, y_val)
-    memory = np.max(memory_usage((pipeline.predict, (X_val,))))
-    
-    return error, memory
+    cv_error =  1 - cross_val_score(pipeline, X_train, y_train, cv=cv,
+                               scoring='accuracy').mean()
+    cv_space = asizeof.asizeof(pipeline)
+    return cv_error, cv_space
 
 # Run optimization
 def run_optimization():
