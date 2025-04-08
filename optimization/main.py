07f9639ea1d214ff8e631032148a47eeba6878c8
noahfrank3 <noahfrank3@outlookc.om>
2025-04-08 00:40:09 -0400

    Minor changes

--- optimization/main.py
+++ optimization/main.py
@@ -8,20 +8,21 @@ from optimization import run_optimization
 from reset_db import reset_db
 
 # Parameters
-hpc_enabled = True # enable HPC
+hpc_enabled = False # enable HPC
 test_db = False # use test database
 do_reset_db = False # reset database
-reset_only = False # only reset database
+reset_only = True # only reset database
 
 k = 5 # number of folds for k-fold CV
 n_trials = 10000 # number of trials for optimization run
-f_trial_workers = 0.65 # fraction of workers dedicated to running whole trials (HPC only)
+# f_trial_workers = 0.65 # fraction of workers dedicated to running whole trials (HPC only)
 
 data_path = Path('..') / 'data' # path of data directory
 
 if hpc_enabled:
     n_workers = cpu_count() # number of CPUs to use
-    n_trial_workers = int(f_trial_workers*n_workers)
+    # n_trial_workers = int(f_trial_workers*n_workers)
+    n_trial_workers = 1
     n_internal_workers = n_workers - n_trial_workers
 else:
     n_trial_workers = 1
@@ -33,7 +34,7 @@ else:
     load_dotenv()
     db_url = os.getenv('DB_URL')
 
-if do_reset_db and not hpc_enabled:
+if (do_reset_db or reset_only) and not hpc_enabled:
     reset_db(db_url)
 
 # Save global parameters
