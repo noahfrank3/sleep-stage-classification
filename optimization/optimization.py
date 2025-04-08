f4961706cbb5bfe4cd8ba1bae546f34d6d199059
noahfrank3 <noahfrank3@outlookc.om>
2025-04-07 22:07:17 -0400

    Change space measure to validation prediction space complexity

--- optimization/optimization.py
+++ optimization/optimization.py
@@ -1,9 +1,9 @@
-import numpy as np
 import h5py
+from memory_profiler import memory_usage
+import numpy as np
 import optuna
 from optuna.study import MaxTrialsCallback
-from pympler import asizeof
-from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
@@ -56,9 +56,6 @@ def objective(trial, global_params):
     # Load global parameters
     n_internal_workers = global_params['n_internal_workers']
     n = global_params['n']
-    cv = global_params['cv']
-    X_trainval = global_params['X_trainval']
-    y_trainval = global_params['y_trainval']
 
     # Feature extraction
     feature_extractor = FeatureExtractor(trial, n_internal_workers)
@@ -90,11 +87,39 @@ def objective(trial, global_params):
     ])
 
     # Calculate objectives
-    cv_error =  1 - cross_val_score(pipeline, X_trainval, y_trainval, cv=cv,
-                               scoring='accuracy').mean()
-    cv_space = asizeof.asizeof(pipeline)*2**(-20) # MB
+    return evaluate_objectives(pipeline, global_params)
 
-    return cv_error, cv_space
+def evaluate_objectives(pipeline, global_params):
+    # Load global parameters
+    k = global_params['k']
+    cv = global_params['cv']
+    X_trainval = global_params['X_trainval']
+    y_trainval = global_params['y_trainval']
+
+    cv_error, cv_memory = np.empty(k), np.empty(k)
+
+    for idx, (train_idx, val_idx) in enumerate(cv.split(X_trainval, y_trainval)):
+        # Split data into training and validation sets
+        X_train, y_train = X_trainval[train_idx], y_trainval[train_idx]
+        X_val, y_val = X_trainval[val_idx], y_trainval[val_idx]
+        
+        # Calculate objectives for this fold
+        error, memory = evaluate_fold_objectives(pipeline, X_train, y_train, X_val, y_val)
+        
+        cv_error[idx] = error
+        cv_memory[idx] = memory
+
+    return np.mean(cv_error), np.max(cv_memory)
+
+def evaluate_fold_objectives(pipeline, X_train, y_train, X_val, y_val):
+    # Train model
+    pipeline.fit(X_train, y_train)
+
+    # Calculate objectives
+    error = 1 - pipeline.score(X_val, y_val)
+    memory = np.max(memory_usage((pipeline.predict, (X_val,))))
+    
+    return error, memory
 
 # Run optimization process
 def run_optimization(global_params):
