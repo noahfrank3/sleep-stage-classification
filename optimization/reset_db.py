661ebf1ff198f2272e8e8207f2c6762996d5fa1d
noahfrank3 <noahfrank3@outlookc.om>
2025-04-08 11:51:11 -0400

    Include error handling

--- optimization/reset_db.py
+++ optimization/reset_db.py
@@ -2,10 +2,13 @@ import optuna
 from optuna.samplers import NSGAIISampler
 
 def reset_db(db_url):
-    optuna.delete_study(
-        study_name='sleep_stage_classification', 
-        storage=db_url
-    )
+    try:
+        optuna.delete_study(
+            study_name='sleep_stage_classification', 
+            storage=db_url
+        )
+    except:
+        pass
 
     study = optuna.create_study(
             study_name='sleep_stage_classification',
