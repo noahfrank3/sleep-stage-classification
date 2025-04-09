21ae2a96bf4c76ecaea74614fc4118a62bb532a4
noahfrank3 <noahfrank3@outlookc.om>
2025-04-09 00:19:04 -0400

    Configure optimization for HPC

--- optimization/main.py
+++ optimization/main.py
@@ -1,9 +1,9 @@
 from optimization import Optimizer
 
 if __name__ == '__main__':
-    hpc_enabled = False
+    hpc_enabled = True
     k = 5
-    n_trials = 5
+    n_trials = 10000
     f_trial_workers = 0.8
 
     optimizer = Optimizer(hpc_enabled, k, n_trials, f_trial_workers)
