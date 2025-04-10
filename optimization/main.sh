7547698d5dcebdb1cfc41480168aa6364b0536c0
noahfrank3 <noahfrank3@outlookc.om>
2025-04-10 01:42:21 -0400

    Minor changes

--- optimization/main.sh
+++ optimization/main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J sleep_stage_optimization
 #SBATCH -o out.log
-#SBATCH --array=0-7
+#SBATCH --array=0-8
 #SBATCH --cpus-per-task=24
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=4G
 
 module load python
 source ~/my_env/bin/activate
