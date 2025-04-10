cb34d28873fef0d7ed17ddbb969c1a8a848c79fb
noahfrank3 <noahfrank3@outlookc.om>
2025-04-09 23:50:12 -0400

    Minor changes

--- optimization/main.sh
+++ optimization/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J sleep_stage_optimization
 #SBATCH -o out.log
-#SBATCH --nodes=5
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=4G
