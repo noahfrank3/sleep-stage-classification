94d1741f14711c492a8296e4d0eceef1c44a16e1
noahfrank3 <noahfrank3@outlookc.om>
2025-04-10 02:31:11 -0400

    Minor changes

--- optimization/main.sh
+++ optimization/main.sh
@@ -2,7 +2,7 @@
 #SBATCH -J sleep_stage_optimization
 #SBATCH -o out.log
 #SBATCH --array=0-9
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=4G
 
