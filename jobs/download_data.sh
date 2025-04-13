6b33f9def6def5bf46ebbf9c2cc8519b0be0d85e
noahfrank3 <noahfrank3@outlookc.om>
2025-04-13 03:19:01 -0400

    Add HPC job for downloading dataset

--- jobs/download_data.sh
+++ jobs/download_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=sleepclf_download_data
 #SBATCH --output=output/download_data.log
+#SBATCH --time=24:00:00
 
-wget -r -N -c -np https://physionet.org/files/sleep-edfx/1.0.0/
+wget -r -N -c -np -P data https://physionet.org/files/sleep-edfx/1.0.0/
